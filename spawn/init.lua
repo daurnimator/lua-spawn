4777340317b8e145254f0d85ddff1319857a8cd5
daurnimator <quae@daurnimator.com>
2015-05-18 16:59:38 +1000

    spawn.init: add 'start' and 'run' functions

--- spawn/init.lua
+++ spawn/init.lua
@@ -1,5 +1,19 @@
 local posix = require "spawn.posix"
 
+local default_file_actions = posix.new_file_actions()
+local default_attr = posix.new_attr()
+
+local function start(program, ...)
+	return posix.spawnp(program, default_file_actions, default_attr,
+		{ program, ... }, {})
+end
+
+local function run(program, ...)
+	return posix.waitpid(start(program, ...))
+end
+
 return {
 	posix = posix;
+	start = start;
+	run = run;
 }
