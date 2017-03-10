3cab7db4f7170e61d4170d81400b8b97d8f891f7
daurnimator <quae@daurnimator.com>
2017-03-10 14:27:25 +1100

    spawn/lua-spawn.h: Remove declarations of removed functions

--- spawn/lua-spawn.h
+++ spawn/lua-spawn.h
@@ -3,9 +3,6 @@
 
 #include <lua.h>
 
-int luaopen_spawn_kill(lua_State *L);
 int luaopen_spawn_posix(lua_State *L);
-int luaopen_spawn_sigset(lua_State *L);
-int luaopen_spawn_wait(lua_State *L);
 
 #endif /* LUASPAWN_H */
